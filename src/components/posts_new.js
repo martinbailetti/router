1a6b927cd5857facf5d2fa31be3d72409a083373
Martin <martinbailetti@gmail.com>
2018-04-05 22:50:49 -0500

    posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import {Link} from 'react-router-dom'; 
+import {connect} from 'react-redux'; 
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
 
@@ -25,7 +27,11 @@ class PostsNew extends Component {
 
 	onSubmit(values){
 
-		console.log(values);
+		this.props.createPost(values, ()=>{
+
+			this.props.history.push("/");
+
+		});
 
 	}
 
@@ -82,4 +88,8 @@ function validate(values){
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew)
+})(
+
+	connect(null, {createPost})(PostsNew)
+
+)
---
+++ src/components/posts_show.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class PostsShow extends Component {
+
+
+
+	render() {
+	    return (
+	      <div>
+	      SHOW
+	      </div>
+	    );
+	}
+}
